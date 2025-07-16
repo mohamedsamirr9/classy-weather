0cecb8cc7b1106dd27ff739bd6d0d5661aeb53cd
mohamedsamirr9 <mohamedsamir979797@gmail.com>
2025-07-16 13:59:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleDecrement() {
+    this.setState((currState) => {
+      return { count: currState.count - 1 };
+    });
+  }
+  handleIncrement() {
+    this.setState((currState) => {
+      return { count: currState.count + 1 };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Counter;
